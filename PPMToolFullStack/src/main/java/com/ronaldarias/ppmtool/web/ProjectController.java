61bef9d5014656b32d173a5d41af038e87d746b2
ronald.arias <ron_1192@hotmail.com>
2019-01-06 18:38:01 -0500

    Se agrego la anotacion Valid y BindingResult para mejorar las validaciones de objeto de la clase Project, ahora el return del metodo createNewProject es generico

--- PPMToolFullStack/src/main/java/com/ronaldarias/ppmtool/web/ProjectController.java
+++ PPMToolFullStack/src/main/java/com/ronaldarias/ppmtool/web/ProjectController.java
@@ -6,9 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/api/project")
@@ -17,8 +20,20 @@ public class ProjectController {
     @Autowired
     private ProjectService projectService;
 
+    /**
+     * @Valid sirve para obtener el error en el json de forma mas clara y detallada
+     * @BindingResult analiza el objeto y determina si tiene o no errores
+     *
+     * @return si result tiene errores entonces devuelve un String con un mensaje, de lo contrario
+     * devuelve un response json tipo Project con los datos ingresados
+     */
     @PostMapping("")
-    public ResponseEntity<Project> createNewProject(@RequestBody Project project) {
+    public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result) {
+
+        if (result.hasErrors()) {
+            return new ResponseEntity<String>("Invalid Object", HttpStatus.BAD_REQUEST);
+        }
+
         projectService.saveOrUpdateProject(project);
         return new ResponseEntity<Project>(project, HttpStatus.CREATED);
     }
