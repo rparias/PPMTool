3f63cfc0a9286e09cd1950931d653f79917ff325
ronald.arias <ron_1192@hotmail.com>
2019-01-06 19:07:12 -0500

    Cuando hay un error en los campos json enviados en el POST el backend retorna un Map con el formato atributo:errormessage

--- PPMToolFullStack/src/main/java/com/ronaldarias/ppmtool/web/ProjectController.java
+++ PPMToolFullStack/src/main/java/com/ronaldarias/ppmtool/web/ProjectController.java
@@ -7,11 +7,15 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/api/project")
@@ -24,14 +28,23 @@ public class ProjectController {
      * @Valid sirve para obtener el error en el json de forma mas clara y detallada
      * @BindingResult analiza el objeto y determina si tiene o no errores
      *
-     * @return si result tiene errores entonces devuelve un String con un mensaje, de lo contrario
+     * @return si result tiene errores entonces devuelve un Map con la
+     * estructura "atributo":"error". De lo contrario
      * devuelve un response json tipo Project con los datos ingresados
      */
     @PostMapping("")
     public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result) {
 
+        // en caso de error hago que el json q me devuelve tenga la estructura "field": "errormessage"
         if (result.hasErrors()) {
-            return new ResponseEntity<String>("Invalid Object", HttpStatus.BAD_REQUEST);
+
+            Map<String, String> errorMap = new HashMap<>();
+
+            for (FieldError error : result.getFieldErrors()) {
+                errorMap.put(error.getField(), error.getDefaultMessage());
+            }
+
+            return new ResponseEntity<Map<String, String>>(errorMap, HttpStatus.BAD_REQUEST);
         }
 
         projectService.saveOrUpdateProject(project);
