4d7ea554c982b7a7ec24d7200419b8b33998ea95
ronald.arias <ron_1192@hotmail.com>
2019-01-14 21:44:45 -0500

    Se agregó una excepcion personalizada para cuando se ingrese un ID que rompa la condicion de Unique

--- PPMToolFullStack/src/main/java/com/ronaldarias/ppmtool/services/ProjectService.java
+++ PPMToolFullStack/src/main/java/com/ronaldarias/ppmtool/services/ProjectService.java
@@ -1,6 +1,7 @@
 package com.ronaldarias.ppmtool.services;
 
 import com.ronaldarias.ppmtool.domain.Project;
+import com.ronaldarias.ppmtool.exceptions.ProjectIdException;
 import com.ronaldarias.ppmtool.repositories.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,7 +13,14 @@ public class ProjectService {
     private ProjectRepository projectRepository;
 
     public Project saveOrUpdateProject(Project project) {
-        return projectRepository.save(project);
+
+        try {
+            project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+            return projectRepository.save(project);
+        } catch (Exception ex) {
+            throw new ProjectIdException("Project ID " + project.getProjectIdentifier().toUpperCase() + " already exists");
+        }
+
     }
 
 }
--- PPMToolFullStack/src/main/java/com/ronaldarias/ppmtool/web/ProjectController.java
+++ PPMToolFullStack/src/main/java/com/ronaldarias/ppmtool/web/ProjectController.java
@@ -8,15 +8,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.validation.Valid;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 @Controller
 @RequestMapping("/api/project")
