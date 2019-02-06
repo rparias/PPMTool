c7c4d456bec31764fd6a2eae5a544dbd79b4cbf7
ronald.arias <ron_1192@hotmail.com>
2019-02-05 21:44:52 -0500

    Creado proyecto nuevo en react

--- PPMToolFullStack/src/main/java/com/ronaldarias/ppmtool/web/ProjectController.java
+++ PPMToolFullStack/src/main/java/com/ronaldarias/ppmtool/web/ProjectController.java
@@ -6,13 +6,12 @@ import com.ronaldarias.ppmtool.services.ProjectService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
-@Controller
+@RestController
 @RequestMapping("/api/project")
 public class ProjectController {
 
